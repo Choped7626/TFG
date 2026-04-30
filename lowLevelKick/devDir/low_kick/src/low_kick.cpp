63299a6e12572553a95f3738175155a6f473e482
Choped7626 <mario.l.a3094@gmail.com>
2026-04-30 12:06:23 +0200

    Updated with more states (not finished)

--- lowLevelKick/devDir/low_kick/src/low_kick.cpp
+++ lowLevelKick/devDir/low_kick/src/low_kick.cpp
@@ -35,22 +35,18 @@ private:
   enum class State {
     IDLE,
     STAND_UP,
+    COME_BACK_UP,
+    ADJUST_REAR,
     STABLE_FR_IN_AIR,
+    STAND_DOWN,
+    WAIT,
   };
 
   State current_state = State::STAND_UP;
   double state_start_time = 0.0;
-
-  void init_cmd() {
-    for (int i = 0; i < 20; i++) {
-      low_cmd.motor_cmd[i].mode = 0x01; // Set toque mode, 0x00 is passive mode
-      low_cmd.motor_cmd[i].q = PosStopF;
-      low_cmd.motor_cmd[i].kp = 0;
-      low_cmd.motor_cmd[i].dq = VelStopF;
-      low_cmd.motor_cmd[i].kd = 0;
-      low_cmd.motor_cmd[i].tau = 0;
-    }
-  }
+  double dt = 0.002;
+  double runing_time = 0.0;
+  double phase = 0.0;
 
   rclcpp::TimerBase::SharedPtr timer_; // ROS2 timer
   rclcpp::Publisher<unitree_go::msg::LowCmd>::SharedPtr
@@ -59,12 +55,65 @@ private:
   unitree_go::msg::LowCmd low_cmd;
 
   double stand_up_joint_pos[12] = {
-      0.00571868, 0.608813, -1.21763, -0.00571868, 0.608813, -1.21763,
-      0.00571868, 0.608813, -1.21763, -0.00571868, 0.608813, -1.21763};
+      // FR
+      0.00571868,
+      0.608813,
+      -1.21763,
+      // FL
+      -0.00571868,
+      0.608813,
+      -1.21763,
+      // RR
+      0.00571868,
+      0.608813,
+      -1.21763,
+      // RL
+      -0.00571868,
+      0.608813,
+      -1.21763,
+  };
 
   double stand_down_joint_pos[12] = {
       0.0473455, 1.22187, -2.44375, -0.0473455, 1.22187, -2.44375,
       0.0473455, 1.22187, -2.44375, -0.0473455, 1.22187, -2.44375};
+
+  double stable_kick_joint_pos[12] = {
+      // FR
+      0.058142,
+      -1.1651,
+      -1.21763,
+      // FL
+      -0.1971,
+      0.5587,
+      -0.9260,
+      // RR
+      -0.2305,
+      0.886,
+      -1.6125,
+      // RL
+      -0.20427,
+      0.965,
+      -1.83890,
+  };
+
+  double adjust_rear_joint_pos[12] = {
+      // FR
+      -0.10,
+      0.6,
+      -1.45,
+      // FL
+      -0.10,
+      0.53,
+      -1.45,
+      // RR
+      -0.2,
+      0.5,
+      -1.37,
+      // RL
+      -0.18,
+      0.56,
+      -1.5,
+  };
 
   //
   // 0-2: FR
@@ -77,19 +126,28 @@ private:
   // [2 5 8 11]: Calf (Gemelo) ----> RANGO: -2.7 a -0.8 (encojida/estirada)
   //
 
-  double stable_kick_joint_pos[12] = {
+  void init_cmd() {
+    for (int i = 0; i < 20; i++) {
+      low_cmd.motor_cmd[i].mode = 0x01; // Set toque mode, 0x00 is passive mode
+      low_cmd.motor_cmd[i].q = PosStopF;
+      low_cmd.motor_cmd[i].kp = 0;
+      low_cmd.motor_cmd[i].dq = VelStopF;
+      low_cmd.motor_cmd[i].kd = 0;
+      low_cmd.motor_cmd[i].tau = 0;
+    }
+  }
 
-      0.01, 0.6,  -2.7,  -0.2,
-      0.7,  -1.4,
-
-      -0.2, 0.5,  -1.17, 0.1,
-      0.6,  -1.4
-
-  };
-
-  double dt = 0.002;
-  double runing_time = 0.0;
-  double phase = 0.0;
+  void change_joints(double joint_arr_dest[12], double joint_arr_org[12],
+                     double phase) {
+    for (int i = 0; i < 12; i++) {
+      low_cmd.motor_cmd[i].q =
+          phase * joint_arr_dest[i] + (1 - phase) * joint_arr_org[i];
+      low_cmd.motor_cmd[i].dq = 0;
+      low_cmd.motor_cmd[i].kp = phase * 60.0 + (1 - phase) * 20.0;
+      low_cmd.motor_cmd[i].kd = 5;
+      low_cmd.motor_cmd[i].tau = 0;
+    }
+  }
 
   void timer_callback() {
 
@@ -97,60 +155,97 @@ private:
     double state_time = runing_time - state_start_time;
 
     switch (current_state) {
+
     case State::STAND_UP: {
       double duration = 3.0;
-      double t = std::min(1.0, state_time / duration);
-      double phase = 0.5 * (1.0 - cos(t * M_PI));
+      phase = tanh(state_time / 1.2);
 
-      for (int i = 0; i < 12; i++) {
-        low_cmd.motor_cmd[i].q = phase * stand_up_joint_pos[i] +
-                                 (1 - phase) * stand_down_joint_pos[i];
-        low_cmd.motor_cmd[i].kp = 40.0;
-        low_cmd.motor_cmd[i].kd = 3.0;
-      }
+      change_joints(stand_up_joint_pos, stand_down_joint_pos, phase);
 
-      if (t >= 1.0) {
+      if (state_time >= duration) {
         current_state = State::STABLE_FR_IN_AIR;
         state_start_time = runing_time;
       }
       break;
     }
+
+    case State::ADJUST_REAR: {
+      double duration = 3.0;
+      phase = tanh(state_time / 1.2);
+
+      change_joints(adjust_rear_joint_pos, stand_up_joint_pos, phase);
+
+      if (state_time >= duration) {
+        current_state = State::STABLE_FR_IN_AIR;
+        state_start_time = runing_time;
+      }
+      break;
+    }
+
     case State::STABLE_FR_IN_AIR: {
       double duration = 4.0;
-      double t = std::min(1.0, state_time / duration);
-      double phase = 0.5 * (1.0 - cos(t * M_PI));
+      double phase = tanh(state_time / 1.8);
 
-      for (int i = 0; i < 12; i++) {
-        low_cmd.motor_cmd[i].q = phase * stand_down_joint_pos[i] +
-                                 (1 - phase) * stand_up_joint_pos[i];
-        low_cmd.motor_cmd[i].kp = 40.0;
-        low_cmd.motor_cmd[i].kd = 3.0;
+      change_joints(stable_kick_joint_pos, stand_up_joint_pos, phase);
+
+      if (state_time >= duration) {
+        current_state = State::WAIT;
+        state_start_time = runing_time;
       }
+      break;
+    }
 
-      if (t >= 1.0) {
+    case State::WAIT: {
+      // do nothing, last position
+      double duration = 5.0;
+
+      printf("WAIT %f\n", state_time);
+
+      if (state_time >= duration){
+        current_state = State::COME_BACK_UP;
+	state_start_time = runing_time;
+      }
+      break;
+    }
+
+    case State::STAND_DOWN: {
+      double duration = 3.0;
+      phase = tanh(state_time / 1.2);
+
+      change_joints(stand_down_joint_pos, stand_up_joint_pos, phase);
+
+      if (state_time >= duration) {
         current_state = State::IDLE;
         state_start_time = runing_time;
       }
       break;
     }
+
     case State::IDLE: {
-      double duration = 2.0;
-      double t = std::min(1.0, state_time / duration);
-      double phase = 0.5 * (1.0 - cos(t * M_PI));
-
-      // for
-
+      // do nothing, last position
       printf("IDLE\n");
+      break;
+    }
 
-      if (t >= 1.0) {
+    case State::COME_BACK_UP: {
+      double duration = 3.0;
+      phase = tanh(state_time / 1.2);
+
+      change_joints(stand_up_joint_pos, stable_kick_joint_pos, phase);
+
+      if (state_time >= duration) {
         current_state = State::IDLE;
         state_start_time = runing_time;
       }
       break;
     }
+
     default:
       break;
     }
+
+    get_crc(low_cmd);            // Check motor cmd crc
+    cmd_puber->publish(low_cmd); // Publish lowcmd message
   }
   /*
     void timer_callback() {
@@ -245,4 +340,3 @@ int main(int argc, char **argv) {
   rclcpp::shutdown();                           // Exit
   return 0;
 }
-
